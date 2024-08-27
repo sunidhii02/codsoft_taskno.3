63453f41b33b799bae2bca1267c0fed6d9412dd0
sunidhii02 <150722038+sunidhii02@users.noreply.github.com>
2024-08-28 01:59:45 +0530

    Password_Generator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,7 +5,6 @@ MAX_LEN = int(input("Enter the range for your password:"))
 
 if MAX_LEN <= 4:
     print("Enter a range greater than 4")
-
 else:
 
     DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -20,12 +19,10 @@ else:
                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOWCASE_CHARACTERS + SYMBOLS
-
     rand_digit = random.choice(DIGITS)
     rand_upper = random.choice(UPCASE_CHARACTERS)
     rand_lower = random.choice(LOWCASE_CHARACTERS)
     rand_symbol = random.choice(SYMBOLS)
-
     temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
 
     for x in range(MAX_LEN - 4):
